b31342616bb56802c94a992bf4712d0105c1c699
Horia Balc <balc_horia@yahoo.com>
2021-08-09 10:43:18 +0300

    localStorage

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 var list = document.querySelector("ul");
-
+var stringList=new Array();
 button.addEventListener('click', () => {
     const textValue = input.value;
     let isInList = false;
     const listElement = document.createElement("LI");
     const buttonDelete = document.createElement("button");
     buttonDelete.innerText = "Delete";
+    
     if (textValue != "") {
         var liItems = list.querySelectorAll("LI");
-        
-        
         liItems.forEach(function (userItem) {
-            
-            localStorage.getItem(userItem);
-            localStorage.setItem(userItem, userItem);
-            console.log(localStorage);
             if (userItem.getAttribute("data-value") === textValue)
                 isInList = true;
         });
@@ -24,18 +19,135 @@ button.addEventListener('click', () => {
         if (!isInList) {
             listElement.setAttribute("data-value", textValue);
             listElement.append(textValue, buttonDelete);
-            /*listElement.innerHTML=textValue;
-            listElement.appendChild(buttonDelete);*/
+            //stringList=localStorage.getItem('stringList');
+           
+            //stringList.push(textValue);
+            //console.log(stringList);
+            
             list.appendChild(listElement);
+            //localStorage.setItem('stringList', stringList);
+            localStorage.setItem('ulEl', ulEl);
             buttonDelete.addEventListener('click', () => {
                 //localStorage.remove();
-                listElement.remove();
+                
+                //stringList.pop();
+                //console.log(stringList);
+               
+                //localStorage.setItem('stringList', stringList);
             })
         }
     }
 
 })
-usersData=[
+
+/*var listToDo =localStorage.getItem('stringList');
+function initList(){
+        const buttonDelete = document.createElement("button");
+        const listElement = document.createElement("LI");
+        buttonDelete.innerText = "Delete";
+
+    
+   for(var i=0;i<listToDo.length;i++){ 
+
+        listElement.append(listToDo[i], buttonDelete);
+        list.appendChild(listElement);
+          
+       
+   } 
+   buttonDelete.addEventListener('click', () => {
+            //localStorage.remove();
+           
+            listToDo.pop(); 
+            listElement.remove();
+            console.log(listToDo);
+            localStorage.setItem('stringList', listToDo);
+        })
+}
+initList();
+*/
+
+
+/////////////////////////////////////////////////////
+
+const THEME1={
+    'primary':'rgb(199, 199, 199)',
+    'header':'rgb(23, 74, 116)',
+    'title':'rgb(243, 239, 239)',
+    'secondaryHeader':'gainsboro',
+}
+
+const THEME2={
+    'primary':'#2a9d8f',
+    'header':'#264653',
+    'title':'#e9c46a',
+    'secondaryHeader':'#f4a261',
+}
+
+const THEME3={
+    'primary':'#84a98c',
+    'header':'#2f3e46',
+    'title':'#52796f',
+    'secondaryHeader':'#cad2c5',
+}
+
+const  dropBtn=document.getElementsByClassName('dropbtn');
+const dropContent=document.getElementById("myDropdown");
+function showBtns(){
+    dropContent.classList.toggle("show");
+}
+const setTheme = theme => {
+    const root = document.documentElement;
+    /*for (const property in theme) {
+        root.style.setProperty(property, theme[property]);
+    }*/
+    root.style.setProperty('--header', theme['header']);
+    root.style.setProperty('--primary', theme['primary']);
+    root.style.setProperty('--title', theme['title']);
+    root.style.setProperty('--secondaryHeader', theme['secondaryHeader']);
+
+}
+
+function chooseTheme(nrTheme){
+var buttons = dropContent.querySelectorAll("button");
+var theme;
+
+/*for(var i=0;i<buttons.length;i++){
+    if(nrTheme==i)
+        theme=themes[i];
+}*/
+        if (nrTheme == 1){
+            theme= THEME1;
+        }
+        if (nrTheme== 2){   
+            theme= THEME2;    
+        }
+        if (nrTheme == 3){
+            theme= THEME3;               
+        }
+        setTheme(theme);
+        localStorage.setItem('Theme', nrTheme);
+}
+
+const initTheme = () => {
+    // const theme = localStorage.getItem('isDarkTheme'); // DARK, LIGHT
+    // const isDarkTheme = theme === 'DARK_THEME';
+    const numberTheme = localStorage.getItem('Theme');
+    chooseTheme(numberTheme);
+}
+
+initTheme();
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////  
+/*usersData=[
     {
         name:'Vasilica',
         age: 23,
@@ -52,8 +164,6 @@ usersData=[
         email: 'vasi_vasi@gmail.com'
     }
 ];
-
-
 const userContainer=document.getElementById("userContainer");
 
 function generateUserCard(user){
@@ -81,4 +191,4 @@ function populateUsersContainer(users){
     });
 }
 
-populateUsersContainer(usersData);
+populateUsersContainer(usersData);*/
